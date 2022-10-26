d736129c9caf7e106d0a973c66ee875fd179550e
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2022-10-25 23:35:50 -0400

    Added pricing page, loader, etc.

--- Outward/static/js/index.js
+++ Outward/static/js/index.js
@@ -11,10 +11,34 @@ var productTitle = products.querySelector("#productTitle");
 var productSplash = products.querySelector("#splash");
 var productContent = products.querySelector("#content");
 
+var pricing = document.querySelector("#pricing");
+var priceTitle = pricing.querySelector("#priceTitle");
+var priceSplash = pricing.querySelector("#splash");
+var priceContent = pricing.querySelector("#content");
+
 var footer = document.querySelector("#footer-show");
 var footer2 = document.querySelector("#footer-scroll");
 
-function fadeZoom(delay1=1500, delay2=15) {
+function fadeZoom(screen, delay1=1500, delay2=15) {
+    let toggle = true;
+    if (nav.classList.contains("wood")) {
+        if (products.style.display == "flex") {
+            if (screen == "products") {
+                return;
+            } else if (screen == "pricing") {
+                delay1 = 0;
+                toggle = false;
+            }
+        }
+        if (pricing.style.display == "flex") {
+            if (screen == "pricing") {
+                return;
+            } else if (screen == "products") {
+                delay1 = 0;
+                toggle = false;
+            }
+        }
+    } 
     nav.classList.add("no-shadow");
     splash.style.opacity = 0;
     for (var i = 0; i < content.children.length; i++) {
@@ -25,19 +49,59 @@ function fadeZoom(delay1=1500, delay2=15) {
     }
     splash.style.transform = "scale(3)";
     setTimeout(function() {
-        nav.classList.toggle("wood");
-        nav.classList.toggle("cove");
+        if (toggle) {
+            nav.classList.toggle("wood");
+            nav.classList.toggle("cove");
+        }
         nav.classList.remove("no-shadow");
+        
         landing.style.display = "none";
-        products.style.display = "flex";
-        productSplash.style.opacity = 1;
+        
+        if (screen == "products") {
+            products.style.display = "flex";
+            productSplash.style.opacity = 1;
+
+            pricing.style.display = "none";
+            priceSplash.style.opacity = 0;
+        } else if (screen == "pricing") {
+            pricing.style.display = "flex";
+            priceSplash.style.opacity = 1;
+
+            products.style.display = "none";
+            productSplash.style.opacity = 0;
+        }
         setTimeout(function() {
-            for (var i = 0; i < productContent.children.length; i++) {
-                productContent.children[i].style.opacity = 1;
-            }
-            for (var i = 0; i < products.children.length; i++) {
-                products.children[i].style.opacity = 1;
-            }
+            if (screen == "products") {
+                window.location.href = "#products";
+                for (var i = 0; i < productContent.children.length; i++) {
+                    productContent.children[i].style.opacity = 1;
+                }
+                for (var i = 0; i < products.children.length; i++) {
+                    products.children[i].style.opacity = 1;
+                }
+
+                for (var i = 0; i < priceContent.children.length; i++) {
+                    priceContent.children[i].style.opacity = 0;
+                }
+                for (var i = 0; i < pricing.children.length; i++) {
+                    pricing.children[i].style.opacity = 0;
+                }
+            } else if (screen == "pricing") {
+                window.location.href = "#pricing";
+                for (var i = 0; i < priceContent.children.length; i++) {
+                    priceContent.children[i].style.opacity = 1;
+                }
+                for (var i = 0; i < pricing.children.length; i++) {
+                    pricing.children[i].style.opacity = 1;
+                }
+
+                for (var i = 0; i < productContent.children.length; i++) {
+                    productContent.children[i].style.opacity = 0;
+                }
+                for (var i = 0; i < products.children.length; i++) {
+                    products.children[i].style.opacity = 0;
+                }
+            }   
         }, delay2);
     }, delay1);
 }
@@ -80,10 +144,20 @@ document.addEventListener('scroll', () => {
     content.style.opacity = opacity;
     if (window.innerWidth <= 1750) {
         title.style.opacity = opacity; 
-        productTitle.style.opacity = opacity;
+        if (products.style.display != "none") {
+            productTitle.style.opacity = opacity;
+        }
+        if (pricing.style.display != "none") {
+            priceTitle.style.opacity = opacity;
+        }
     } else {
         title.style.opacity = 1;
-        productTitle.style.opacity = 1;
+        if (products.style.display != "none") {
+            productTitle.style.opacity = 1;
+        }
+        if (pricing.style.display != "none") {
+            priceTitle.style.opacity = 1;
+        }
     }
     footer2.style.setProperty("--opacity", opacity);
 
@@ -94,6 +168,19 @@ window.addEventListener('load', function() {
     document.getElementById("footer-copyright-2").innerHTML = "Copyright &copy" + new Date().getFullYear() + " | Outward Industries&trade;";
 
     if (window.location.href.split("/#")[1] == "products") {
-        fadeZoom(0, 0);
+        fadeZoom("products", 0, 0);
     } 
+    if (window.location.href.split("/#")[1] == "pricing") {
+        fadeZoom("pricing", 0, 0);
+    } 
+    loader();
 });
+
+// Loader
+function loader() {
+    var loader = document.querySelector(".loader");
+    loader.style.opacity = 1;
+    loader.style.display = "flex";
+    $(".loader").fadeOut(2000);
+    $(".loader-span").fadeOut(1000);
+}
